cc9a6e36acd51b7a95208ea454bbf7a826bfc9bb
MiniMinja <minyoungheo.heo@gmail.com>
2022-07-11 20:02:49 -0700

    Update _2021jrc1.java

--- ACSL/src/_2021jrc1.java
+++ ACSL/src/_2021jrc1.java
@@ -1,5 +1,19 @@
 import java.util.*;
 
 public class _2021jrc1 {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void init() {
+		
+	}
+	
+	public static void solve() {
+		
+	}
+	
+	public static void output() {
+		
+	}
 }
